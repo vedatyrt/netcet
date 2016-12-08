b9534d45f64b29a6ca0c847002305eefd3e228ec
agtokty <agitoktay@gmail.com>
2016-12-08 23:12:25 +0300

    first modification

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ the [Getting Started](http://socket.io/get-started/chat/) guide
 of the Socket.IO website.
 
 Please refer to it to learn how to run this application.
+
+#netçet
--- index.js
+++ index.js
@@ -2,16 +2,22 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+
+var path = __dirname + '/views/';
+
+app.get('/', function(req, res) {
+    res.sendFile(path + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function(socket) {
+    socket.on('chat', function(msg) {
+        io.emit('chat', msg);
+    });
+    socket.on('connected', function(msg) {
+        io.emit('connected', msg);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
